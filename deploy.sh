54ba03698115461010279e3492f9fa69d1cc0dde
Lucas Tosello Latini <lucastosellolatini@gmail.com>
2025-07-04 22:17:11 -0300

    FIX docker command

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,14 @@ echo "🚀 Iniciando despliegue en el servidor..."
 
 # 1. Descargar la última versión de la imagen desde Docker Hub.
 echo "  -> Descargando la nueva imagen..."
-docker-compose pull
+# Usamos el nuevo comando sin guion
+docker compose pull
 
 # 2. Levantar los contenedores con la nueva imagen.
 # Docker Compose es lo suficientemente inteligente para recrear solo los que cambiaron.
 echo "  -> Reiniciando los servicios..."
-docker-compose up -d
+# Usamos el nuevo comando sin guion
+docker compose up -d
 
 # 3. Limpiar imágenes viejas y sin usar para liberar espacio.
 echo "  -> Limpiando imágenes antiguas..."
